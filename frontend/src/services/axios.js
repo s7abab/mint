eb4621d0a8c7a5ea2e822506476d26348daf3fc1
SHABAB <123226241+s7abab@users.noreply.github.com>
2023-09-25 13:13:43 +0530

    env-done

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const Api = axios.create({ baseURL: "http://localhost:8080" });
+const Api = axios.create({ baseURL: import.meta.env.VITE_SOCKET_URL });
 
 Api.interceptors.request.use((req) => {
   if (localStorage.getItem("token")) {
