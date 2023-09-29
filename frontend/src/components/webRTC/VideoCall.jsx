d0d65c7cb9fce371b97065d3b93b04547ba07705
SHABAB <123226241+s7abab@users.noreply.github.com>
2023-09-29 14:02:36 +0530

    video-call-improved

--- frontend/src/components/webRTC/VideoCall.jsx
+++ frontend/src/components/webRTC/VideoCall.jsx
@@ -1,315 +1,211 @@
-import React, {
-  useContext,
-  useMemo,
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import ReactPlayer from "react-player";
 import peer from "../../services/peer";
 import socket from "../../services/socket";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-let a = true;
-let b = true;
-function VideoCall({ data, close }) {
+import { Button } from "@material-tailwind/react";
+import { AiOutlineAudioMuted } from "react-icons/ai";
+import { FcEndCall } from "react-icons/fc";
+import { BsCameraVideo } from "react-icons/bs";
+
+const VideoCall = () => {
+  const [remoteSocketId, setRemoteSocketId] = useState(null);
   const [myStream, setMyStream] = useState();
-  const [remote, setRemote] = useState();
-  const [camera, setCamera] = useState(true);
-  const [audio, setAudio] = useState(true);
-  const [video, setVideo] = useState(true);
-  const ref = useRef();
-  const remotRef = useRef();
+  const [remoteStream, setRemoteStream] = useState();
+  const [isAudioMuted, setIsAudioMuted] = useState(true);
+  const [isCameraOff, setIsCameraOff] = useState(true);
+  const role = useSelector((state) => state.auth.role);
 
-  useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({ video: a, audio: b })
-      .then((streams) => {
-        ref.current.srcObject = streams;
-        setMyStream(streams);
-      });
+  const navigate = useNavigate();
+
+  const handleUserJoined = useCallback(({ email, id }) => {
+    console.log(`Email ${email} joined room`);
+    setRemoteSocketId(id);
   }, []);
 
-  useEffect(() => {
-    socket.emit("user:entered", { room: data?.room });
-  }, []);
-
-  // console.log(data)
-
-  const sendOffer = useCallback(
-    async (msg) => {
-      try {
-        console.log(msg);
-        const offer = await peer.getOffer();
-        socket.emit("user:sendoffer", { offer, room: data?.room });
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    [socket]
-  );
-
-  const recieveoffer = useCallback(
-    async (offer) => {
-      try {
-        console.log(offer);
-        const answer = await peer.getAnswer(offer);
-        socket.emit("user:sendAns", { answer, room: data?.room });
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    [socket]
-  );
-
-  const acceptAns = useCallback(
-    async (answer) => {
-      try {
-        console.log(myStream?.getTracks());
-        await peer.acceptAnswer(answer);
-        for (const track of myStream?.getTracks()) {
-          peer.peer.addTrack(track, myStream);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    [myStream]
-  );
-
-  useEffect(() => {
-    peer.peer.addEventListener("negotiationneeded", async () => {
-      const offer = await peer.getOffer();
-      socket.emit("user:sendoffer", { offer, room: data?.room });
+  const handleCallUser = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
     });
-    return () => {
-      peer.peer.removeEventListener("negotiationneeded", async () => {
-        const offer = await peer.getOffer();
-        socket.emit("user:sendoffer", { offer, room: data?.room });
+    const offer = await peer.getOffer();
+    socket.emit("user:call", { to: remoteSocketId, offer });
+    setMyStream(stream);
+  }, [remoteSocketId, socket]);
+
+  const handleIncommingCall = useCallback(
+    async ({ from, offer }) => {
+      setRemoteSocketId(from);
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
       });
+      setMyStream(stream);
+      console.log(`Incoming Call`, from, offer);
+      const ans = await peer.getAnswer(offer);
+      socket.emit("call:accepted", { to: from, ans });
+    },
+    [socket]
+  );
+
+  const sendStreams = useCallback(() => {
+    for (const track of myStream.getTracks()) {
+      peer.peer.addTrack(track, myStream);
+    }
+  }, [myStream]);
+
+  const handleCallAccepted = useCallback(
+    ({ from, ans }) => {
+      peer.setLocalDescription(ans);
+      console.log("Call Accepted!");
+      sendStreams();
+    },
+    [sendStreams]
+  );
+
+  const handleNegoNeeded = useCallback(async () => {
+    const offer = await peer.getOffer();
+    socket.emit("peer:nego:needed", { offer, to: remoteSocketId });
+  }, [remoteSocketId, socket]);
+
+  useEffect(() => {
+    peer.peer.addEventListener("negotiationneeded", handleNegoNeeded);
+    return () => {
+      peer.peer.removeEventListener("negotiationneeded", handleNegoNeeded);
     };
+  }, [handleNegoNeeded]);
+
+  const handleNegoNeedIncomming = useCallback(
+    async ({ from, offer }) => {
+      const ans = await peer.getAnswer(offer);
+      socket.emit("peer:nego:done", { to: from, ans });
+    },
+    [socket]
+  );
+
+  const handleNegoNeedFinal = useCallback(async ({ ans }) => {
+    await peer.setLocalDescription(ans);
   }, []);
 
   useEffect(() => {
     peer.peer.addEventListener("track", async (ev) => {
-      let stream = ev.streams;
-      setRemote(stream[0]);
-      // remotRef.current.srcObject = stream[0];
-      console.log(stream);
+      const remoteStream = ev.streams;
+      console.log("GOT TRACKS!!");
+      setRemoteStream(remoteStream[0]);
     });
-
-    // peer.peer.addEventListener("")
   }, []);
 
-  const handleToggleCamera = async () => {
-    if (!a) {
-      await ref.current.srcObject?.getVideoTracks()[0]?.stop();
-    }
-    if (!b) {
-      await ref.current.srcObject?.getAudioTracks()[0]?.stop();
-    }
-
-    navigator.mediaDevices
-      .getUserMedia({ video: a, audio: b })
-      .then((streams) => {
-        setMyStream(streams);
-        ref.current.srcObject = streams;
-      });
-  };
-
-  const handleToggleCameraWhileCalling = async () => {
-    try {
-      await ref.current.srcObject?.getVideoTracks()[0]?.stop();
-      await ref.current.srcObject?.getAudioTracks()[0]?.stop();
-    } catch (err) {
-      console.log(err);
-    } finally {
-      await navigator.mediaDevices
-        .getUserMedia({ video: a, audio: b })
-        .then((streams) => {
-          console.log("working");
-          setMyStream(streams);
-          ref.current.srcObject = streams;
-        })
-        .catch((err) => {
-          console.log(err);
-          setMyStream();
-        });
-    }
-    // try {
-    //  await peer.peer.getSenders().forEach(sender =>  peer.peer.removeTrack());
-
-    //   for (const track of ref.current.srcObject?.getTracks()) {
-    //     peer.peer.addTrack(track, ref.current.srcObject);
-    //   }
-    // } catch (err) {
-    //   console.log(err);
-    // }
-  };
-
   useEffect(() => {
-    socket.on("user:joined", sendOffer);
-    socket.on("user:recieveoffer", recieveoffer);
-    socket.on("user:acceptAns", acceptAns);
+    socket.on("user:joined", handleUserJoined);
+    socket.on("incomming:call", handleIncommingCall);
+    socket.on("call:accepted", handleCallAccepted);
+    socket.on("peer:nego:needed", handleNegoNeedIncomming);
+    socket.on("peer:nego:final", handleNegoNeedFinal);
 
     return () => {
-      socket.off("user:joined", sendOffer);
-      socket.off("user:recieveoffer", recieveoffer);
-      socket.off("user:acceptAns", acceptAns);
+      socket.off("user:joined", handleUserJoined);
+      socket.off("incomming:call", handleIncommingCall);
+      socket.off("call:accepted", handleCallAccepted);
+      socket.off("peer:nego:needed", handleNegoNeedIncomming);
+      socket.off("peer:nego:final", handleNegoNeedFinal);
     };
-  }, [socket, sendOffer, recieveoffer, acceptAns]);
+  }, [
+    socket,
+    handleUserJoined,
+    handleIncommingCall,
+    handleCallAccepted,
+    handleNegoNeedIncomming,
+    handleNegoNeedFinal,
+  ]);
+
+  // Function to toggle camera off
+  const toggleCamera = () => {
+    if (myStream) {
+      const videoTrack = myStream.getVideoTracks()[0];
+      if (videoTrack) {
+        videoTrack.enabled = !isCameraOff;
+        setIsCameraOff(!isCameraOff);
+      }
+    }
+  };
+
+  // Function to toggle audio mute
+  const toggleAudioMute = () => {
+    if (myStream) {
+      const audioTrack = myStream.getAudioTracks()[0];
+      if (audioTrack) {
+        audioTrack.enabled = !isAudioMuted;
+        setIsAudioMuted(!isAudioMuted);
+      }
+    }
+  };
+
+  const handleHangUp = () => {
+    if (myStream) {
+      myStream.getTracks().forEach((track) => track.stop());
+    }
+    setMyStream(null);
+    setRemoteStream(null);
+    socket.emit("hangup");
+    if (role === "user") {
+      navigate("/bookings");
+    } else if (role === "counselor") {
+      navigate("/counselor/bookings");
+    }
+  };
+
+  if(remoteSocketId){
+    handleCallUser()
+  }
 
   return (
-    <div className="flex flex-col justify-center height-100vh">
-      {video ? (
-        <div className="flex justify-between flex-wrap self-center border-black border-solid border md:w-1/3 p-5">
-          <div>
-            <div className="myStream-div">
-              <video ref={ref} className="myStream" autoPlay mute playsInline />
-            </div>
-            <div className="flex justify-between mt-4 ">
-              {camera ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  fill="currentColor"
-                  class="bi bi-camera"
-                  viewBox="0 0 16 16"
-                  onClick={() => {
-                    a = false;
-                    handleToggleCamera();
-                    setCamera(!camera);
-                  }}
-                >
-                  {" "}
-                  <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z" />{" "}
-                  <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />{" "}
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  onClick={() => {
-                    a = true;
-                    handleToggleCamera();
-                    setCamera(!camera);
-                  }}
-                  stroke-linejoin="round"
-                  class="feather feather-camera-off"
-                >
-                  <line x1="1" y1="1" x2="23" y2="23"></line>
-                  <path d="M21 21H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h3m3-3h6l2 3h4a2 2 0 0 1 2 2v9.34m-7.72-2.06a4 4 0 1 1-5.56-5.56"></path>
-                </svg>
-              )}
-
-              {!audio ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  fill="currentColor"
-                  class="bi bi-mic-mute"
-                  viewBox="0 0 16 16"
-                  onClick={() => {
-                    b = true;
-                    handleToggleCamera();
-                    setAudio(!audio);
-                  }}
-                >
-                  {" "}
-                  <path d="M13 8c0 .564-.094 1.107-.266 1.613l-.814-.814A4.02 4.02 0 0 0 12 8V7a.5.5 0 0 1 1 0v1zm-5 4c.818 0 1.578-.245 2.212-.667l.718.719a4.973 4.973 0 0 1-2.43.923V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 1 0v1a4 4 0 0 0 4 4zm3-9v4.879l-1-1V3a2 2 0 0 0-3.997-.118l-.845-.845A3.001 3.001 0 0 1 11 3z" />{" "}
-                  <path d="m9.486 10.607-.748-.748A2 2 0 0 1 6 8v-.878l-1-1V8a3 3 0 0 0 4.486 2.607zm-7.84-9.253 12 12 .708-.708-12-12-.708.708z" />{" "}
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  fill="currentColor"
-                  class="bi bi-mic"
-                  viewBox="0 0 16 16"
-                  onClick={() => {
-                    b = false;
-                    handleToggleCamera();
-                    setAudio(!audio);
-                  }}
-                >
-                  {" "}
-                  <path d="M3.5 6.5A.5.5 0 0 1 4 7v1a4 4 0 0 0 8 0V7a.5.5 0 0 1 1 0v1a5 5 0 0 1-4.5 4.975V15h3a.5.5 0 0 1 0 1h-7a.5.5 0 0 1 0-1h3v-2.025A5 5 0 0 1 3 8V7a.5.5 0 0 1 .5-.5z" />{" "}
-                  <path d="M10 8a2 2 0 1 1-4 0V3a2 2 0 1 1 4 0v5zM8 0a3 3 0 0 0-3 3v5a3 3 0 0 0 6 0V3a3 3 0 0 0-3-3z" />{" "}
-                </svg>
-              )}
-            </div>
-          </div>
-          <div className="flex items-center ms-3">
-            <button
-              className=" text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-              onClick={() => {
-                setVideo(false);
-                try {
-                  for (const track of ref.current.srcObject?.getTracks()) {
-                    peer.peer.addTrack(track, myStream);
-                  }
-                } catch (err) {
-                  console.log(err);
-                }
-              }}
-            >
-              Start
-            </button>
-          </div>
-        </div>
-      ) : (
-        <div className="flex items-center justify-center bg-gray-100">
-          <div className="bg-white rounded-lg shadow-lg p-4 w-full">
-            <div className="aspect-w-16 aspect-h-9">
-              {/* Local video stream */}
-              <video
-                ref={ref}
-                className="bg-black myVideo  rounded-lg"
-                autoPlay
-                muted
-              ></video>
-            </div>
-            <div className="mt-4 w-full flex justify-center">
-              {/* Remote video stream */}
-              <ReactPlayer url={remote} height="70vh" width="80%" playing />
-            </div>
-            <div className="mt-4 flex justify-center space-x-4">
-              <button
-                className="px-4 py-2 bg-red-500 text-white rounded-md"
-                onClick={async () => {
-                  if (ref.current.srcObject?.getVideoTracks()[0]) {
-                    await ref.current.srcObject?.getVideoTracks()[0].stop();
-                  }
-
-                  if (ref.current.srcObject?.getAudioTracks()[0]) {
-                    await ref.current.srcObject?.getAudioTracks()[0].stop();
-                  }
-
-                  peer.peer.close();
-                  close();
-                  window.location.reload();
-                }}
-              >
-                End Call
-              </button>
-              <div className="flex justify-between mt-4 "></div>
-            </div>
-          </div>
-        </div>
-      )}
-      {/* <ReactPlayer playing muted width="500px" height="500px" url={myStream} /><br />
-      {remote ? <ReactPlayer playing muted width="500px" height="500px" url={remote} />:<h1>wait</h1>} */}
+    <div className="h-screen bg-gray-800  w-screen flex flex-col items-center justify-center relative">
+      <h4 className="text-green-900">{remoteSocketId && "Connected"}</h4>
+      <div className="absolute top-5 left-5 z-50">
+        {myStream && <Button onClick={sendStreams}>Send Stream</Button>}
+        {/* {remoteSocketId && !remoteStream && <Button onClick={handleCallUser}>CALL</Button>} */}
+      </div>
+      <div className="rounded h-screen relative">
+        {remoteStream && (
+          <ReactPlayer playing height="100%" width="100%" url={remoteStream} />
+        )}
+      </div>
+      <div className="absolute w-1/5 bottom-16 right-0 border-2 border-black">
+        {myStream && (
+          <ReactPlayer playing height="100%" width="100%" url={myStream} />
+        )}
+      </div>
+      <div className="fixed bottom-0 left-0 right-0 z-10 bg-gray-900 p-4 text-white flex justify-center">
+        {isCameraOff ? (
+          <BsCameraVideo
+            className="text-5xl mr-5 bg-gray-700 text-white rounded-full p-2  hover:text-white cursor-pointer"
+            onClick={toggleCamera}
+          />
+        ) : (
+          <BsCameraVideo
+            className="text-5xl mr-5 bg-red-500 text-white rounded-full p-2  hover:text-white cursor-pointer"
+            onClick={toggleCamera}
+          />
+        )}
+        {isAudioMuted ? (
+          <AiOutlineAudioMuted
+            className="text-5xl mr-5 bg-gray-700 text-white rounded-full p-2  hover:text-white cursor-pointer"
+            onClick={toggleAudioMute}
+          />
+        ) : (
+          <AiOutlineAudioMuted
+            className="text-5xl mr-5 bg-red-500 text-white rounded-full p-2  hover:text-white cursor-pointer"
+            onClick={toggleAudioMute}
+          />
+        )}
+        <FcEndCall
+          className="text-5xl mr-5 bg-gray-700 text-white rounded-full p-2 hover:bg-red-500 hover:text-white cursor-pointer"
+          onClick={handleHangUp}
+        />
+      </div>
     </div>
   );
-}
-
+};
 export default VideoCall;
