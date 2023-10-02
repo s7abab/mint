90db111e782be33da90b44cc2733cb882e491206
Shabab <s7abab2k1@gmail.com>
2023-10-02 16:51:55 +0530

    image-bug-fixed

--- frontend/src/components/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "@material-tailwind/react";
-
-export const Loading = () => {
-  return (
-    <div className="">
-      <Spinner className="h-12 w-12 absolute top-1/2 left-1/2" />
-    </div>
-  );
-};
--- frontend/src/services/socket.js
+++
@@ -1,5 +0,0 @@
-import io from "socket.io-client";
-
-const socket = io(import.meta.env.VITE_SOCKET_URL);
-
-export default socket;
