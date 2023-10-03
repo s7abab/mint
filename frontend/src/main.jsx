62f9ea45a27662e20112c7ba732dea3446b9f2b2
Shabab <s7abab2k1@gmail.com>
2023-10-03 15:46:17 +0530

    error-boundary-added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,13 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
 import { Provider } from "react-redux";
 import store from "./redux/store.jsx";
+import ErrorBoundary from "./pages/error/ErrorBoundary.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
       <React.StrictMode>
         <ThemeProvider>
-          <App />
+          <ErrorBoundary>
+            <App />
+          </ErrorBoundary>
         </ThemeProvider>
       </React.StrictMode>
     </Provider>
---
+++ frontend/src/pages/error/ErrorBoundary.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    // Handle the error, log it, and set the state to indicate an error.
+    this.setState({ hasError: true });
+    console.error(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Render a fallback UI when an error occurs.
+      return <div>Something went wrong.</div>;
+    }
+    return this.props.children; // Render the child components normally.
+  }
+}
+
+export default ErrorBoundary;
